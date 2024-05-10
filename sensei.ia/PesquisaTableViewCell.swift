a2b3751b3ea34665142a4dd1f73ccedde659e9c1
guilherme <guilhermemleitte@hotmail.com>
2024-05-09 20:04:54 -0700

    Adicionando tela de pesquisa

---
+++ sensei.ia/PesquisaTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PesquisaTableViewCell.swift
+//  sensei.ia
+//
+//  Created by user262081 on 5/9/24.
+//
+
+import UIKit
+
+class PesquisaTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
